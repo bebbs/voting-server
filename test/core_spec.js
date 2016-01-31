60baf14fbbc0518ed6cbec25b9220f4393fa3edd
Josh Bebbington <josh@focalwebsites.com>
2016-01-31 17:55:20 +0000

    When one entry is left, declare it the victor and congratulate it

--- test/core_spec.js
+++ test/core_spec.js
@@ -64,11 +64,30 @@ describe('application logic', () => {
         entries: List.of('Sunshine', 'Millions', '127 Hours')
       });
       const nextState = next(state);
+      
       expect(nextState).to.equal(Map({
         vote: Map({
           pair: List.of('Sunshine', 'Millions')
         }),
         entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+      }));
+    });
+    
+    it('marks winners when just one entry left', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = next(state);
+      
+      expect(nextState).to.equal(Map({
+        winner: 'Trainspotting'
       }));
     });
   });
